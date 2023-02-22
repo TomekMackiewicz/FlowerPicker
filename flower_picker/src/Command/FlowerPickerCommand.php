6863f1691d947bf16d71cf9a8a7ca1bf4d9c9d5b
Tomek <tomasz.mackiewicz79@gmail.com>
2023-02-22 16:51:07 +0100

    Create command

---
+++ flower_picker/src/Command/FlowerPickerCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+// the name of the command is what users type after "php bin/console"
+#[AsCommand(
+    name: 'app:flower-picker',
+    description: 'Picks three random flowers'
+)]
+class FlowerPickerCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setHelp('This command allows you to pick three random flowers...');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+
+        // or return this if some error happened during the execution
+        // (it's equivalent to returning int(1))
+        // return Command::FAILURE;
+
+        // or return this to indicate incorrect command usage; e.g. invalid options
+        // or missing arguments (it's equivalent to returning int(2))
+        // return Command::INVALID
+    }
+}
