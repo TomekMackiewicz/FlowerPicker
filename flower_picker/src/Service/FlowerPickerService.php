666b51624af20a0e1d3d16cdea9903a105c6d260
Tomek <tomasz.mackiewicz79@gmail.com>
2023-02-22 19:32:23 +0100

    Refactor FlowerPickerService

--- flower_picker/src/Service/FlowerPickerService.php
+++ flower_picker/src/Service/FlowerPickerService.php
@@ -5,27 +5,18 @@ declare(strict_types=1);
 namespace App\Service;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Symfony\Component\HttpClient\CurlHttpClient;
 use Symfony\Component\DomCrawler\Crawler;
 
 class FlowerPickerService
 {
-    public function __construct(private readonly HttpClientInterface $client)
-    {
-        //private readonly $client = $client;
+    public function __construct(
+        private readonly HttpClientInterface $client
+    ) {
     }
 
     public function fetchWebsiteInformation(): array
     {
-        $client = new CurlHttpClient();
-        $html = $client->request('GET', 'https://sklep.swiatkwiatow.pl', [
-            // ...
-            'extra' => [
-                'curl' => [
-                    //CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V6,
-                ],
-            ],
-        ]);
+        $html = $this->client->request('GET', 'https://sklep.swiatkwiatow.pl', []);
         $crawler = new Crawler($html->getContent());
         $images = $crawler->filterXPath('//img[contains(@class, "primary")]')->extract(['src', 'alt']);
 
