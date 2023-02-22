4dadfd628f700ba347bdc04cf530d7b07eb020a1
Tomek <tomasz.mackiewicz79@gmail.com>
2023-02-22 17:00:24 +0100

    Flower picker test

---
+++ flower_picker/tests/Command/FlowerPickerCommandTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Command;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class FlowerPickerCommandTest extends KernelTestCase
+{
+    public function testExecute()
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('app:flower-picker');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+
+        $commandTester->assertCommandIsSuccessful();
+    }
+}
