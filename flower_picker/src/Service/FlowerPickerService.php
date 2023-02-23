4251571adfd9eae8ca3c6e0ff55c274066ed7c3d
Tomek <tomasz.mackiewicz79@gmail.com>
2023-02-23 09:13:51 +0100

    Refactor FlowerService

--- flower_picker/src/Service/FlowerPickerService.php
+++ flower_picker/src/Service/FlowerPickerService.php
@@ -6,36 +6,100 @@ namespace App\Service;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\DomCrawler\Crawler;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Flower;
 
 class FlowerPickerService
 {
     private const NUMBER_OF_IMAGES = 3;
+    private $entityManager;
 
     public function __construct(
-        private readonly HttpClientInterface $client
+        private readonly HttpClientInterface $client,
+        private readonly ManagerRegistry $doctrine
     ) {
+        $this->entityManager = $doctrine->getManager();
     }
 
     public function fetchWebsiteInformation(): array
     {
-        $html = $this->client->request('GET', 'https://sklep.swiatkwiatow.pl', []);
-        $crawler = new Crawler($html->getContent());
-        $images = $crawler->filterXPath('//img[contains(@class, "primary")]')->extract(['src', 'alt']);
+        $images = $this->fetchImages();
+        $flowersHashes = $this->getFlowersHashes();
+        $this->removeDuplicates($images, $flowersHashes);
+        $randomKeys = $this->pickRandomFlowers($images);
 
-        $randomKeys = array_rand($images, self::NUMBER_OF_IMAGES);
-        $randomImages = [];
-
+        // Save images
+        $flowers = [];
         foreach ($randomKeys as $key) {
-            $randomImages[] = $images[$key];
+            $flower = new Flower();
+            $flower->setSrc($images[$key][0]);
+            $flower->setAlt($images[$key][1]);
+            $flower->setHash(md5($images[$key][0]));
+            $this->entityManager->persist($flower);
+            $flowers[] = $flower;
         }
-        
-dd($randomImages);
+
+        foreach ($flowers as $flower) {
+            $imageUrl = $flower->getSrc();
+            @$rawImage = file_get_contents($imageUrl);
+            if ($rawImage) {
+                file_put_contents($flower->getAlt().'.jpg', $rawImage);
+            }
+        }
+
+        $this->entityManager->flush();
+
         return $images;
     }
 
-    // Dodanie do listy wylosowanych kwiatów
+    /**
+     * Fetch images from website
+     * @return array
+     */
+    private function fetchImages(): array
+    {
+        $html = $this->client->request('GET', 'https://sklep.swiatkwiatow.pl', []);
+        $crawler = new Crawler($html->getContent());
 
-    // Pobranie listy już wybranych kwiatów
+        return $crawler->filterXPath('//img[contains(@class, "primary")]')->extract(['src', 'alt']);
+    }
 
-    // Wylosowanie 3 wcześniej niewybranych kwiatów
+    /**
+     * Get downloaded images
+     * @return array
+     */
+    private function getFlowersHashes(): array
+    {
+        return $this->doctrine->getRepository(Flower::class)->getHash();
+    }
+
+    /**
+     * Check for duplicated images and remove if any
+     * @param array $images
+     * @param array $flowersHashes
+     */
+    private function removeDuplicates(array &$images, array $flowersHashes): void
+    {
+        foreach ($images as $key => $image) {
+            if (in_array(md5($image[0]), $flowersHashes)) {
+                unset($images[$key]);
+            }
+        }
+    }
+
+    /**
+     * Pick random flowers
+     * @param array $images
+     * @return array
+     */
+    private function pickRandomFlowers(array $images): array
+    {
+        return count($images) < self::NUMBER_OF_IMAGES ?
+            array_keys($images) : array_rand($images, self::NUMBER_OF_IMAGES);
+    }
+
+    private function saveImages()
+    {
+
+    }
 }
